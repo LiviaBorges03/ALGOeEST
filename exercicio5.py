7add930576248e76d3603ad076a2e0151d284d28
Lívia Borges Ferreira <liviabf02@gmail.com>
2025-03-27 17:13:10 -0400

    Add files via upload

---
+++ exercicio5.py
@@ -0,0 +1,7 @@
+numero = int(input("insira um numero"))
+if numero >0:
+    print("o {numero} é positivo")
+if numero <0:
+    print ("o {numero} é negativo")
+else:
+    print("o {numero} é 0")
---
+++ exercicio6.py
@@ -0,0 +1,3 @@
+idade= int(input("insira sua idade"))
+print("maior de idade") if idade >= 18 else print ("menor de idade")
+           
